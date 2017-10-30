4d12b51caea0ced9a582294c47ab5259e058351b
ausaleh <abubakarsalehusman@gmail.com>
2017-10-30 12:01:26 +0300

    step 2

---
+++ contactus.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Isacomgroup</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- my CSS -->
+<link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css" />
+    <link rel="stylesheet" href="css/contact.css">
+  </head>
+  <body>
+    <!--  navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="img/logo.jpg" alt="isacom logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Products
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Real estate</a>
+            <a class="dropdown-item" href="#">Furnitures</a>
+            <a class="dropdown-item" href="#"> construction materials</a>
+          </div>
+        </li><li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Service
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">IT support</a>
+              <a class="dropdown-item" href="#">Plumber</a>
+              <a class="dropdown-item" href="#">Electrician</a>
+              <a class="dropdown-item" href="#">Architecture</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Fundi</a>
+            </div>
+          </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Photos and videos</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contact Us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            My Account
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="signin.php">Sign in</a>
+              <a class="dropdown-item" href="signup.php">Sign up</a>
+
+          </div>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 pull-right">
+     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+   </form>
+  </div>
+</nav>
+<!-- body -->
+
+
+    <div class="container">
+
+        <div class="wrapper animated bounceInLeft">
+            <div class="company-info">
+                <ul>
+                    <li><h1>IsacomGroup</h1></li>
+                    <li><i class="fa fa-road"></i> Usiu road</li>
+                    <li><i class="fa fa-phone"></i>+254 555 555 555</li>
+                    <li><i class="fa fa-envelope"></i>Isacomgroup@test.com</li>
+                </ul>
+            </div>
+            <div class="contact">
+                <h3>Email Us</h3>
+                <form>
+                    <p>
+                        <label>Name</label>
+                        <input type="text" name="name">
+                    </p>
+                    <p>
+                        <label>company</label>
+                        <input type="text" name="company">
+                    </p>
+                    <p>
+                        <label>Email Address</label>
+                        <input type="text" name="email">
+                    </p>
+                    <p>
+                        <label>Phone Number</label>
+                        <input type="text" name="phone">
+                    </p>
+
+                      <div class="form-group">
+                        <label>Message</label>
+                        <textarea class="form-control"rows="3"></textarea>
+                      </div>
+
+                    <p class="full pull-right">
+                        <button>Submit</button>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="js/request.js"></script>
+
+
+
+
+
+
+<!-- end body -->
+<!--Footer-->
+<footer class="page-footer #424242 grey darken-3">
+    <!--Footer Links-->
+    <div class="container">
+        <!-- Footer links -->
+        <div class="row text-center text-md-left mt-3 pb-3">
+
+            <!--First column-->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Company name</h6>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <!--/.First column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Second column-->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Products</h6>
+                <p><a href="#!">Real Estate</a></p>
+                <p><a href="#!">Furnitures</a></p>
+                <p><a href="#!">Construction</a></p>
+            </div>
+            <!--/.Second column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Third column-->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Useful links</h6>
+                <p><a href="#!">Your Account</a></p>
+                <p><a href="#!">Become an Affiliate</a></p>
+                <p><a href="#!">Shipping Rates</a></p>
+                <p><a href="#!">Help</a></p>
+            </div>
+            <!--/.Third column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Fourth column-->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Contact</h6>
+                <p><i class="fa fa-home mr-3"></i> Uganda</p>
+                <p><i class="fa fa-envelope mr-3"></i> Isacomgroup@example.com</p>
+                <p><i class="fa fa-phone mr-3"></i> +2340800-00-00</p>
+                <p><i class="fa fa-print mr-3"></i> +2540-000-00-00</p>
+            </div>
+            <!--/.Fourth column-->
+
+        </div>
+        <!-- Footer links -->
+
+        <hr>
+
+        <div class="row py-3 d-flex align-items-center">
+
+            <!--Grid column-->
+            <div class="col-md-8 col-lg-9">
+
+                <!--Copyright-->
+                <p class="text-center text-md-left grey-text">© 2017 Copyright: <a href="https://www.Boa.com"><strong> Isacomgroup.com</strong></a></p>
+                <!--/.Copyright-->
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 col-lg-3 ml-lg-0">
+
+                <!--Social buttons-->
+                <div class="social-section text-center text-md-left">
+
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-google-plus"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-linkedin"></i></a>
+
+                </div>
+                <!--/.Social buttons-->
+
+            </div>
+            <!--Grid column-->
+
+        </div>
+
+    </div>
+
+</footer>
+<!--/.Footer-->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Isacomgroup</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- my CSS -->
+<link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+    <!--  navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="img/logo.jpg" alt="isacom logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Products
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="realestate.php">Real estate</a>
+            <a class="dropdown-item" href="#">Furnitures</a>
+            <a class="dropdown-item" href="#"> construction materials</a>
+          </div>
+        </li><li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Service
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">IT support</a>
+              <a class="dropdown-item" href="#">Plumber</a>
+              <a class="dropdown-item" href="#">Electrician</a>
+              <a class="dropdown-item" href="#">Architecture</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Fundi</a>
+            </div>
+          </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Photos and videos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.php">Contact Us</a>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            My Account
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="signin.php">Sign in</a>
+              <a class="dropdown-item" href="signup.php">Sign up</a>
+
+          </div>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 pull-right">
+     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+   </form>
+  </div>
+</nav>
+<!-- body -->
+
+<!--slider-->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="img/realestate/slider4.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="img/realestate/slider5.jpg" width="100%" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="img/realestate/slider1.jpg" width="100%" alt="Third slide">
+    </div>
+  <div class="carousel-item">
+    <img class="d-block img-fluid" src="img/realestate/arc.png" width="100%" alt="forth slide">
+  </div>
+<div class="carousel-item">
+  <img class="d-block img-fluid" src="img/realestate/slider3.jpg" width="100%" alt="fifth slide">
+  </div>
+</div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--end of slider-->
+<div class="content">
+
+
+  <h1>The Isacom Group</h1>
+  <p padding-right="250px" padding-left="250px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+
+</div>
+
+<!--Footer-->
+<footer class="page-footer #424242 grey darken-3">
+    <!--Footer Links-->
+    <div class="container">
+        <!-- Footer links -->
+        <div class="row text-center text-md-left mt-3 pb-3">
+
+            <!--First column-->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Company name</h6>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <!--/.First column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Second column-->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Products</h6>
+                <p><a href="#!">Real Estate</a></p>
+                <p><a href="#!">Furnitures</a></p>
+                <p><a href="#!">Construction</a></p>
+            </div>
+            <!--/.Second column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Third column-->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Useful links</h6>
+                <p><a href="#!">Your Account</a></p>
+                <p><a href="#!">Become an Affiliate</a></p>
+                <p><a href="#!">Shipping Rates</a></p>
+                <p><a href="#!">Help</a></p>
+            </div>
+            <!--/.Third column-->
+
+            <hr class="w-100 clearfix d-md-none">
+
+            <!--Fourth column-->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="title mb-4 font-bold">Contact</h6>
+                <p><i class="fa fa-home mr-3"></i> Uganda</p>
+                <p><i class="fa fa-envelope mr-3"></i> Isacomgroup@example.com</p>
+                <p><i class="fa fa-phone mr-3"></i> +2340800-00-00</p>
+                <p><i class="fa fa-print mr-3"></i> +2540-000-00-00</p>
+            </div>
+            <!--/.Fourth column-->
+
+        </div>
+        <!-- Footer links -->
+
+        <hr>
+
+        <div class="row py-3 d-flex align-items-center">
+
+            <!--Grid column-->
+            <div class="col-md-8 col-lg-9">
+
+                <!--Copyright-->
+                <p class="text-center text-md-left grey-text">© 2017 Copyright: <a href="https://www.Boa.com"><strong> Isacomgroup.com</strong></a></p>
+                <!--/.Copyright-->
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 col-lg-3 ml-lg-0">
+
+                <!--Social buttons-->
+                <div class="social-section text-center text-md-left">
+
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-google-plus"></i></a>
+                        <a class="btn-floating btn-sm rgba-white-slight mr-xl-4" href="#"><i class="fa fa-linkedin"></i></a>
+
+                </div>
+                <!--/.Social buttons-->
+
+            </div>
+            <!--Grid column-->
+
+        </div>
+
+    </div>
+
+</footer>
+<!--/.Footer-->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
